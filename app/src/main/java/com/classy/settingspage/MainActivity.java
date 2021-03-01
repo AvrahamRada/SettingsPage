a7b2d8126b54f992fbf1859644b210ef7d32bce0
Avraham Rada <avraham1992rada@gmail.com>
2021-03-01 05:39:40 +0200

    Fix some bugs

--- app/src/main/java/com/classy/settingspage/MainActivity.java
+++ app/src/main/java/com/classy/settingspage/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.classy.settingspagelibrary.Basic.BasicSettings;
 import com.classy.settingspagelibrary.CheckBox.Checkbox;
@@ -16,39 +17,29 @@ import com.classy.settingspagelibrary.Image.Image;
 import com.classy.settingspagelibrary.MainSettings;
 import com.classy.settingspagelibrary.Seekbar.Seekbar;
 import com.classy.settingspagelibrary.Switch.Switch;
+import com.google.android.material.button.MaterialButton;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int REQUEST_CODE_ACTIVITY_SETTINGS = 1001;
-
+    private MaterialButton main_BTN_click;
     public static final String MY_SETTINGS = "MY_SETTINGS";
-    public static final String DEFAULT_VALUE_EDIT_TEXT = "default value";
-
-    private static final String LIST_DIALOG_ONE = "one";
-    private static final String LIST_DIALOG_TWO = "two";
-    private static final String LIST_DIALOG_THREE = "three";
-    private static final String LIST_DIALOG_FOUR = "four";
-    private static final String LIST_DIALOG_FIVE = "five";
-
-    public static final String SETTINGS_KEY_BASIC = "SETTINGS_KEY_BASIC";
-    public static final String SETTINGS_KEY_RINGTONE = "SETTINGS_KEY_RINGTONE";
-    public static final String SETTINGS_KEY_CHECKBOX = "SETTINGS_KEY_CHECKBOX";
-    public static final String SETTINGS_KEY_SWITCH = "SETTINGS_KEY_SWITCH";
-    public static final String SETTINGS_KEY_SEEKBAR = "SETTINGS_KEY_SEEKBAR";
-    public static final String SETTINGS_KEY_EDIT_TEXT = "SETTINGS_KEY_EDIT_TEXT";
-    public static final String SETTINGS_KEY_LIST_SINGLE_CHOICE = "SETTINGS_KEY_LIST_SINGLE_CHOICE";
-    public static final String SETTINGS_KEY_CUSTOM_DIALOG = "SETTINGS_KEY_CUSTOM_DIALOG";
-    public static final String SETTINGS_KEY_CUSTOM_LIST_DIALOG = "SETTINGS_KEY_CUSTOM_LIST_DIALOG";
-    public static final String SETTINGS_KEY_LIST_MULTI_CHOICE = "SETTINGS_KEY_LIST_MULTI_CHOICE";
-    public static final String SETTINGS_KEY_CUSTOM = "SETTINGS_KEY_CUSTOM";
-
     private ArrayList<BasicSettings> mySettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+        main_BTN_click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                intent.putExtra(MY_SETTINGS, mySettings);
+                startActivityForResult(intent, SETTINGS);
+            }
+        });
 
         mySettings = MainSettings.createSettingsArray(
                 new Title("General")                // TITLE
@@ -112,35 +103,19 @@ public class MainActivity extends AppCompatActivity {
                         .build());
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        switch (item.getItemId())
-        {
-            case R.id.settings:
-
-                Intent intent = new Intent(this, SettingsActivity.class);
-                intent.putExtra(MY_SETTINGS, mySettings);
-                startActivityForResult(intent, REQUEST_CODE_ACTIVITY_SETTINGS);
-                return true;
-
-            default:
-                return super.onOptionsItemSelected(item);
-        }
+    private void init() {
+        main_BTN_click = findViewById(R.id.main_BTN_click);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CODE_ACTIVITY_SETTINGS &&
+
+        if (requestCode == SETTINGS &&
                 resultCode == RESULT_OK) {
             mySettings = (ArrayList<BasicSettings>) data.getSerializableExtra(SettingsActivity.INTENT_EXTRA_RESULT);
         }
+        super.onActivityResult(requestCode, resultCode, data);
     }
+    private static final int SETTINGS = 1001;
+
 }
--- app/src/main/java/com/classy/settingspage/SettingsActivity.java
+++ app/src/main/java/com/classy/settingspage/SettingsActivity.java
@@ -1,35 +1,42 @@
 package com.classy.settingspage;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.classy.settingspagelibrary.Basic.BasicSettings;
 import com.classy.settingspagelibrary.CheckBox.CheckboxSettings;
+import com.classy.settingspagelibrary.HeaderAndContent.HeaderAndContentSettings;
+import com.classy.settingspagelibrary.Image.ImageSettings;
 import com.classy.settingspagelibrary.MainSettings;
+import com.classy.settingspagelibrary.Seekbar.Seekbar;
+import com.classy.settingspagelibrary.Seekbar.SeekbarSettings;
+import com.classy.settingspagelibrary.Switch.SwitchSettings;
+import com.classy.settingspagelibrary.Title.TitleSettings;
 
 import java.util.ArrayList;
 
 public class SettingsActivity extends AppCompatActivity {
-    private static final int REQUEST_CODE_NOTIFICATION_PICKER = 1000;
     public static final String INTENT_EXTRA_RESULT = "INTENT_EXTRA_RESULT";
-    public static final String DEFAULT_NOTIFICATION_SUMMARY = "Silent";
-    public static final String SETTINGS_RINGTONE_SILENT_VALUE = "";
-    private Toast mToast;
     private ArrayList<BasicSettings> mySettings;
 
-    CheckBox test;
+
+    TextView tempTitle;
+    TextView tempContent;
+    CheckBox tempCheckbox;
+    Switch tempSwitch;
+    Seekbar tempSeekbar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
@@ -37,26 +44,110 @@ public class SettingsActivity extends AppCompatActivity {
         mySettings = (ArrayList<BasicSettings>) getIntent().getSerializableExtra(MainActivity.MY_SETTINGS);
         MainSettings.inflateSettingsLayout(this, container, mySettings);
 
-        if (mySettings.get(7) instanceof CheckboxSettings){
-            test = findViewById(((CheckboxSettings) mySettings.get(7)).getCheckboxId());
-            test.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    TextView test1 = findViewById(((CheckboxSettings) mySettings.get(7)).getTextViewContentId());
-                    if(test.isChecked() == true){
-                        test1.setText("On");
-                    }
-                    else{
-                        test1.setText("Off");
-                    }
+
+        for (int i = 0; i < mySettings.size(); i++) {
+//            switch (mySettings.get(i).getType()) {
+                if(mySettings.get(i) instanceof TitleSettings) {
+//                    Log.d("PTTT", ((TitleSettings) mySettings.get(i)).getTitle() + "");
                 }
-            });
+
+            if(mySettings.get(i) instanceof HeaderAndContentSettings) {
+//                    Log.d("PTTT", ((HeaderAndContentSettings) mySettings.get(i)).getTitle() + "");
+            }
+
+            if(mySettings.get(i) instanceof CheckboxSettings) {
+//                    Log.d("PTTT", ((CheckboxSettings) mySettings.get(i)).getTitle() + "");
+                tempCheckbox = findViewById(((CheckboxSettings) mySettings.get(i)).getCheckboxId());
+                tempContent = findViewById(((CheckboxSettings) mySettings.get(i)).getTextViewContentId());
+
+                tempCheckbox.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String content = tempContent.getText().toString();
+                        if (tempCheckbox.isChecked() == true) {
+                            tempContent.setText(tempContent.getText() + " - On");
+                        } else {
+                            tempContent.setText(tempContent.getText() + " - Off");
+                        }
+                    }
+                });
+            }
+
+            if(mySettings.get(i) instanceof SwitchSettings) {
+            }
+
+            else if(mySettings.get(i) instanceof ImageSettings) {
+            }
+            else if(mySettings.get(i) instanceof SeekbarSettings) {
+            }
+
+            else{
+
+            }
+
         }
+
+        for (BasicSettings obj : mySettings) {
+            int i = 0;
+            switch (obj.getType()) {
+                case TITLE:
+
+                    break;
+                case CONTENT:
+
+                    break;
+                case CHECKBOX:
+//                    test = findViewById(((CheckboxSettings) mySettings.get(i)).getCheckboxId());
+//                    TextView test1 = findViewById(((CheckboxSettings) mySettings.get(i)).getTextViewContentId());
+//                    test.setOnClickListener(new View.OnClickListener() {
+//                        @Override
+//                        public void onClick(View v) {
+//
+//                            if(test.isChecked() == true){
+//                                test1.setText("On");
+//                            }
+//                            else{
+//                                test1.setText("Off");
+//                            }
+//                        }
+//                    });
+
+                    break;
+                case SWITCH:
+
+                    break;
+                case IMAGE:
+
+                    break;
+                case SEEKBAR:
+
+                    break;
+
+                default:
+                    break;
+            }
+            i++;
+        }
+
+//        if (mySettings.get(7) instanceof CheckboxSettings){
+//            test = findViewById(((CheckboxSettings) mySettings.get(7)).getCheckboxId());
+//            test.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    TextView test1 = findViewById(((CheckboxSettings) mySettings.get(7)).getTextViewContentId());
+//                    if(test.isChecked() == true){
+//                        test1.setText("On");
+//                    }
+//                    else{
+//                        test1.setText("Off");
+//                    }
+//                }
+//            });
+//        }
     }
 
     @Override
-    public void onBackPressed()
-    {
+    public void onBackPressed() {
         Intent results = new Intent();
         results.putExtra(INTENT_EXTRA_RESULT, mySettings);
         setResult(RESULT_OK, results);
